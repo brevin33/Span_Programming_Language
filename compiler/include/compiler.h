d8aab7e2721b01a93afecf90f15dc00e14d733e3
brevin33 <brevin.bell0@gmail.com>
2025-06-28 08:16:28 -0500

    restart because things got to messy

--- compiler/include/compiler.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "parser.h"
-#include "compiler/llvm-c.h"
-
-void compile(projectId projectId);
--- parser/include/parser/struct.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "parser/arena.h"
-#include "parser/type.h"
-
-
-typedef struct
